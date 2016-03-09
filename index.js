c7a5bba532ad567e4fe14f9aa190adbe50e690a1
jitendrapawar <jitendrapawar66@gmail.com>
2016-03-09 17:11:14 +0530

    Online user List DONE
    Now we can view the online user list & also it will get dynamically
    updated...

--- index.js
+++ index.js
@@ -8,23 +8,34 @@ app.get('/',function(req, res){
 });
 
 io.on('connection', function(socket){
+    //console.log(socket);
+    var room_name = "";
+    
+    socket.on("join", function(data){ 
+        room_name = data.room_name;
+        socket.join(data.room_name);
+        console.log("User Joined to room name"+room_name);
+    });
     
     socket.on("New User", function(user_name){
+        //console.log(socket);
         console.log(user_name+" user joined chat");
-        io.emit("New User Added", user_name);
+        socket.user_name = user_name;
+        io.to(room_name).emit("New User Added", {'user_name' : user_name});
     });
     
     console.log("New user connected");
     
     socket.on('Chat Message', function(msg_json){
        //console.log("Message : "+msg_json.msg); 
-       io.emit('Chat Message',msg_json);
+       //io.emit('Chat Message',msg_json);
+       io.to(room_name).emit('Chat Message',msg_json);
     }) ;
     
     socket.on('disconnect', function(obj){
-        console.log(obj);
-       console.log("User disconnected"); 
-       io.emit("disconnect");
+        //console.log(obj);
+        console.log(socket.user_name+" user disconnected"); 
+        io.to(room_name).emit("disconnect",{'user_name' : socket.user_name});
     });
 });
 
